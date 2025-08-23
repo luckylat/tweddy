b92970a8df0dfa8027f7a3927a87c3c77e12ed74
luckylat <40878560+luckylat@users.noreply.github.com>
2025-08-23 23:17:43 +0900

    delete unuses

--- internal/style/colors.go
+++ internal/style/colors.go
@@ -13,21 +13,10 @@ type ApplicationColors struct {
 	BorderColor      color.NRGBA
 }
 
-// TabBarColors defines the color scheme for tab bars
-type TabBarColors struct {
-	Background       color.NRGBA
-	ActiveTab        color.NRGBA
-	InactiveTab      color.NRGBA
-	ActiveText       color.NRGBA
-	InactiveText     color.NRGBA
-	CloseButton      color.NRGBA
-	CloseButtonHover color.NRGBA
-}
 
 // Theme combines all color configurations for the application
 type Theme struct {
 	Application ApplicationColors
-	TabBar      TabBarColors
 }
 
 // DefaultTheme returns the default theme for the application
@@ -42,20 +31,6 @@ func DefaultTheme() Theme {
 			ButtonHover:      color.NRGBA{R: 72, G: 93, B: 114, A: 255},  // Lighter blue-gray button hover
 			BorderColor:      color.NRGBA{R: 180, G: 180, B: 180, A: 255}, // Medium gray border
 		},
-		TabBar: TabBarColors{
-			Background:       color.NRGBA{R: 220, G: 220, B: 220, A: 255}, // Light gray background for contrast
-			ActiveTab:        color.NRGBA{R: 70, G: 130, B: 180, A: 255},  // Steel blue for active tab
-			InactiveTab:      color.NRGBA{R: 190, G: 190, B: 190, A: 255}, // Medium gray for inactive tab
-			ActiveText:       color.NRGBA{R: 255, G: 255, B: 255, A: 255}, // White text for active tab
-			InactiveText:     color.NRGBA{R: 60, G: 60, B: 60, A: 255},    // Dark gray text for inactive tab
-			CloseButton:      color.NRGBA{R: 120, G: 120, B: 120, A: 255}, // Medium gray close button
-			CloseButtonHover: color.NRGBA{R: 220, G: 60, B: 60, A: 255},   // Bright red close button on hover
-		},
 	}
 }
 
-// DefaultTabBarColors returns the default color scheme for tab bars
-// Deprecated: Use DefaultTheme().TabBar instead
-func DefaultTabBarColors() TabBarColors {
-	return DefaultTheme().TabBar
-}
